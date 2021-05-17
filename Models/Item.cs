4df901d891a3f5aa6fdcda49efbd6819c8f18bf9
Robert Marczak <robert.m.1305@gmail.com>
2021-05-17 23:07:04 +0100

    Added Item class.
    Added Item class with initial properties so it can be migrated to the database.

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogADoc.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
