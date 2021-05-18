52fa4b682a8c5aa1244316494edcfcb02007062e
Robert Marczak <robert.m.1305@gmail.com>
2021-05-18 23:30:26 +0100

    Add An Item Controller
    Added an item controller.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using LogADoc.Data;
+using LogADoc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogADoc.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
