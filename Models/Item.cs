f17fd12c72bcc2af9eeb041be754c0a8c8009673
Robert Marczak <robert.m.1305@gmail.com>
2021-05-18 23:32:57 +0100

    Add Missing Properties To The Item Model
    Added 5 missing properties to the model class. These are: CurrentHolder, ReturnedOn, DestroyedOn, RequestedBy, RequestReason

--- Models/Item.cs
+++ Models/Item.cs
@@ -13,7 +13,10 @@ namespace LogADoc.Models
         public string Name { get; set; }
         public DateTime DateAdded { get; set; }
         public string Status { get; set; }
-
-
+        public string CurrentHolder { get; set; }
+        public DateTime ReturnedOn { get; set; }
+        public DateTime DestroyedOn { get; set; }
+        public string RequestedBy { get; set; }
+        public string RequestReason { get; set; }
     }
 }
