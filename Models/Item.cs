d639a7ecced228ef7e710d0b9a84b9fb21db3417
Robert Marczak <robert.m.1305@gmail.com>
2021-05-19 00:31:25 +0100

    Set The Display Name
    Decorated the properties so the more appropriate name is displayed to the user instead of a property name

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,9 +11,12 @@ namespace LogADoc.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Item Name")]
         public string Name { get; set; }
         public DateTime DateAdded { get; set; }
+        [DisplayName("Status")]
         public string Status { get; set; }
+        [DisplayName("Current Holder")]
         public string CurrentHolder { get; set; }
         public DateTime ReturnedOn { get; set; }
         public DateTime DestroyedOn { get; set; }
