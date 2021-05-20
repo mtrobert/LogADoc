5187e17be8e5c926ab3470d958a658fa20091efa
Robert Marczak <robert.m.1305@gmail.com>
2021-05-20 23:35:21 +0100

    Add Actions To The Status Controller
    Added GET and POST actions. That includes delete, create and update.

--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -21,5 +21,94 @@ namespace LogADoc.Controllers
             IEnumerable<Status> objList = _db.Statoos;
             return View(objList);
         }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Status obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET-Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Statoos.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Statoos.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        //GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Statoos.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Status obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }
